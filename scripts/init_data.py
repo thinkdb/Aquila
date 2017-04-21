74abb00b8e97c4916cb1181e3a4d4ac65e76a054
thinkdb <996846239@qq.com>
2017-04-21 17:51:18 +0800

    init

--- scripts/init_data.py
+++ scripts/init_data.py
@@ -11,18 +11,27 @@ django.setup()
 from cmdb import models
 
 # 初始化角色
-models.UserRole.objects.create(user_role_name='dba', user_role_jd='数据库管理员')
-models.UserRole.objects.create(user_role_name='qa', user_role_jd='测试')
-models.UserRole.objects.create(user_role_name='dev', user_role_jd='开发')
-models.UserRole.objects.create(user_role_name='admin', user_role_jd='管理员')
-models.UserRole.objects.create(user_role_name='users', user_role_jd='普通员工,默认值')
+models.UserRole.objects.create(id=1, user_role_name='admin', user_role_jd='管理员')
+models.UserRole.objects.create(id=2, user_role_name='dba', user_role_jd='数据库管理员')
+models.UserRole.objects.create(id=3, user_role_name='qa', user_role_jd='测试')
+models.UserRole.objects.create(id=4, user_role_name='dev', user_role_jd='开发')
+models.UserRole.objects.create(id=5, user_role_name='users', user_role_jd='普通员工,默认值')
 
 
 # 初始化用户组
-models.UserGroup.objects.create(user_group_name='user', user_group_jd='普通用户组, 默认组')
-models.UserGroup.objects.create(user_group_name='admin', user_group_jd='管理员组')
-models.UserGroup.objects.create(user_group_name='dba', user_group_jd='数据库管理员组')
+models.UserGroup.objects.create(id=1, user_group_name='admin', user_group_jd='管理员组')
+models.UserGroup.objects.create(id=2, user_group_name='user', user_group_jd='普通用户组, 默认组')
+models.UserGroup.objects.create(id=3, user_group_name='dba', user_group_jd='数据库管理员组')
 
 # 初始化主机组
-models.HostGroup.objects.create(host_group_name='db', host_group_jd='数据主机组')
-models.HostGroup.objects.create(host_group_name='java', host_group_jd='java主机组')
+models.HostGroup.objects.create(id=1, host_group_name='db', host_group_jd='数据主机组')
+models.HostGroup.objects.create(id=2, host_group_name='java', host_group_jd='java主机组')
+
+# 初始化管理员用户
+models.UserInfo.objects.create(
+    id=1,
+    user_name='admin',
+    user_pass='123456',
+    user_emails='996846239@qq.com',
+    role_id=1,
+    user_group_id=1)
